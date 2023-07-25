c918373dc71949eec596ea2f3c082ce5df244cd4
Edison Li <47466335+edison4354@users.noreply.github.com>
2023-07-25 15:21:53 -0700

    Built landing page components and added custom tailwind styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+        <div className="flex justify-center items-center h-max flex-col-reverse lg:flex-row">
+            <div className="flex justify-center items-center bg-cover lg:h-screen w-screen bg-hero-pattern lg:w-11/20">
+                <svg className="fill-white max-h-96" viewBox="0 0 24 24">
+                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                </svg>
+            </div>
+            <div className="flex justify-center items-start bg-black min-h-max lg:h-screen w-screen p-4 2xl:items-center lg:w-9/20 lg:justify-start lg:min-w-608">
+                <div className="p-5 text-white">
+                    <div className='pb-3'>
+                        <svg className="fill-white h-12" viewBox="0 0 24 24">
+                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                        </svg>
+                    </div>
+                    <div className='my-12 max-w-lg lg:max-w-none'>
+                        <p className='text-7xl font-extrabold'>Happening now</p>
+                    </div>
+                    <div className='mt-12 mb-8'>
+                        <p className='text-4xl font-extrabold'>Join Twitter today.</p>
+                    </div>
+                    <div className='flex flex-col items-start'>
+                        <button className='bg-white text-black rounded-full w-72 py-2 my-2 font-bold'>Sign up with Google</button>
+                        <button className='bg-white text-black rounded-full w-72 py-2 my-2 font-bold'>Sign up with Apple</button>
+                        <p className='text-slate-500'>------------------ or ------------------</p>
+                        <button className='bg-sky-500 text-white rounded-full w-72 py-2 my-2 font-bold'>Create Account</button>         
+                    </div>
+                    <p className='text-xxs w-72'>By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.</p>
+                    <div className='mt-14'>
+                        <p className='text-lg font-extrabold'>Already have an account?</p>
+                    </div>
+                    <button className='outline outline-slate-600 outline-1 text-sky-500 rounded-full w-72 py-2 my-1 font-bold'>Sign in</button>    
+                </div>
+            </div>
+        </div>
+        <div className="bg-black h-20">
+        </div>
     </div>
+
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+            'hero-pattern': "url('https://abs.twimg.com/sticky/illustrations/lohp_en_1302x955.png')",
+        },
+        width: {
+            '11/20': '55%',
+            '9/20': '45%',
+        },
+        fontSize: {
+            'xxs':'10px'
+        },
+        minWidth: {
+            '608': '608px',
+        }
+    },
   },
   plugins: [],
 }
